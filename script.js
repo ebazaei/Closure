668caa4621192b839f68ee2365f796d5c80c2dae
Your Name <youremail@domain.com>
2023-08-20 09:14:54 +0200

    Adder

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Languages string data object
+
 //Example 1 - Translator
+console.log("---Translator---");
 
 var languagesData = {
     "fa": {
@@ -24,3 +26,23 @@ var englishTranslator = createTranslator("en");
 
 console.log(persianTranslator("hi"));
 console.log(englishTranslator("hi"));
+
+//Example 2
+console.log("---Adder---");
+
+function createAdder(size){
+    function add(y){
+        return y + size;
+    }
+    return add;
+}
+
+var addTwo = createAdder(2);
+var addTen = createAdder(10);
+
+addTwo(6); //6+2 = 8
+addTen(5); //5+10 =15
+
+var res = createAdder(8)(6);  //8+6 = 14
+
+console.log(res);
